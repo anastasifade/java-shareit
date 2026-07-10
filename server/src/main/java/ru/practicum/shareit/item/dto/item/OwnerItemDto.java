19384f319f8d091729c3305b137bb33ad494cfd6
anastasifade <anastasifade@gmail.com>
2026-07-10 15:29:50 +0300

    minor fix

--- server/src/main/java/ru/practicum/shareit/item/dto/item/OwnerItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/item/OwnerItemDto.java
@@ -5,7 +5,6 @@ import ru.practicum.shareit.booking.dto.ResponseBookingDto;
 import ru.practicum.shareit.item.dto.comment.ResponseCommentDto;
 
 import java.util.Collection;
-import java.util.Objects;
 
 @Getter
 public final class OwnerItemDto extends ResponseItemDto {
@@ -20,16 +19,4 @@ public final class OwnerItemDto extends ResponseItemDto {
         this.nextBooking = nextBooking;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        OwnerItemDto that = (OwnerItemDto) o;
-        return Objects.equals(lastBooking, that.lastBooking) && Objects.equals(nextBooking, that.nextBooking);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), lastBooking, nextBooking);
-    }
 }
