c3f2550a3ad1cb641e22f796c1591addd8654a70
anastasifade <anastasifade@gmail.com>
2026-07-10 15:14:06 +0300

    fix: checkstyle

--- server/src/test/java/ru/practicum/shareit/item/ItemDtoJsonTests.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemDtoJsonTests.java
@@ -11,9 +11,7 @@ import ru.practicum.shareit.booking.model.BookingStatus;
 import ru.practicum.shareit.item.dto.item.ItemNameDto;
 import ru.practicum.shareit.item.dto.item.OwnerItemDto;
 import ru.practicum.shareit.item.dto.item.ResponseItemDto;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.dto.ResponseUserDto;
-import ru.practicum.shareit.user.model.User;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -101,5 +99,4 @@ public class ItemDtoJsonTests {
         assertThat(content).extractingJsonPathValue("$.lastBooking").hasFieldOrProperty("start");
         assertThat(content).extractingJsonPathValue("$.lastBooking").hasFieldOrProperty("end");
     }
-
 }
