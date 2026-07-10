578d2c2d348c1fc7ed43d8faa87313d1886605c7
anastasifade <anastasifade@gmail.com>
2026-07-10 10:44:05 +0300

    fix: remove unused exceptions from server

--- server/src/main/java/ru/practicum/shareit/base/exceptions/InvalidQueryParameterException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.base.exceptions;
-
-public class InvalidQueryParameterException extends RuntimeException {
-    public InvalidQueryParameterException(String message) {
-        super(message);
-    }
-}
--- server/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -39,12 +39,6 @@ public final class ErrorHandler {
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handle(final InvalidQueryParameterException e) {
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handle(final NotFoundException e) {
         if (e instanceof UserValidationException) {
