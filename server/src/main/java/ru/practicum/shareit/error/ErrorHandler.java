3f0f01a991763d955b60d8ccc095978336600cfe
anastasifade <anastasifade@gmail.com>
2026-07-10 15:27:45 +0300

    add test

--- server/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -2,29 +2,16 @@ package ru.practicum.shareit.error;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.base.exceptions.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 @Slf4j
 @RestControllerAdvice("ru.practicum.shareit")
 public final class ErrorHandler {
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handle(final MethodArgumentNotValidException e) {
-        List<String> errors = new ArrayList<>();
-        e.getBindingResult()
-                .getAllErrors()
-                .forEach(error -> errors.add(error.getDefaultMessage()));
-        return new ErrorResponse(errors.toString());
-    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -41,9 +28,6 @@ public final class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handle(final NotFoundException e) {
-        if (e instanceof UserValidationException) {
-            log.debug("User validation failed.");
-        }
         return new ErrorResponse(e.getMessage());
     }
 
